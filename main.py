b904df64b07f26be1c0740cd0e815a275bbe3550
RZC <1765752538@qq.com>
2025-03-23 22:49:50 +0800

    添加主窗口和历史记录界面的UI文件及其对应的Python实现

---
+++ main.py
@@ -0,0 +1,14 @@
+from PyQt6 import QtWidgets
+from Ui_MainWindow import Ui_MainWindow
+
+def main():
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec())
+
+if __name__ == "__main__":
+    main()
